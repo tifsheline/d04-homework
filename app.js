c2d647b32d5437bc98e830959614ad01e1344241
Tiffany Glover <tif.sheline@gmail.com>
2016-08-19 08:57:20 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,46 @@
+for(var i = 0; i < 300; i+=1){
+  console.log(4 * (i - 3))
+}
+
+var evenBdays = 16
+console.log(evenBdays)
+
+var classMates = [
+  {name: "Anil"},
+  {name: "Greg"},
+  {name: "Kevin"},
+  {name: "Melvin"},
+  {name: "Sandeep"},
+  {name: "Troy"},
+  {name: "Alex"},
+  {name: "Andy"},
+  {name: "David"},
+  {name: "Garrett"},
+  {name: "Phil"},
+  {name: "Roland"},
+  {name: "Sundiata"},
+]
+for(var i = 0; i < classMates.length; i+=1)
+  {console.log(classMates[i])
+}
+
+for(var i = 0; i < 500; i+=1){
+  console.log("Tiffany is awesome!")
+}
+
+for(var i = 0; i < 250; i+=1){
+  console.log("looping")
+}
+alert("Looping complete!")
+
+for(var i = 0; i < 300; i+=1){
+  console.log(299+300)
+}
+
+for(var i = 0; i < 100; i+=1){
+  if(i / 2) {
+  console.log("even");
+} else {
+    console.log("odd");
+  }
+}
